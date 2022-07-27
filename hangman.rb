280658c5d0f6d1812e8cce62f0d1d37e3553baea
Dan <en07djc@icloud.com>
2022-07-27 16:21:14 +0100

    Created choose_word function

--- hangman.rb
+++ hangman.rb
@@ -1,15 +1,31 @@
 puts "Dictionary loaded."
 
+@i = 0
 @game_words = []
 
-word = File.readlines('google-10000-english-no-swears.txt')
-word.each do |word|
-  if word.length > 5 && word.length < 12
-    @game_words << word
+def choose_word
+  word = File.readlines('google-10000-english-no-swears.txt')
+  word.each do |word|
+    if word.length > 5 && word.length < 12
+      @game_words << word
+    end
   end
+
+  array_index = Random.rand(0...6976)
+
+  @computer_word = @game_words[array_index]
 end
 
-array_index = Random.rand(0...6976)
+choose_word
 
-computer_word = @game_words[array_index]
+letter_count = @computer_word.length 
 
+p @computer_word.split(%r{\s*})
+
+
+
+puts "Guess a letter."
+
+#while @i < 9
+
+#end
